c9f5400de659e232300b6adad722c949cda9b672
Christopher Hare <chris.hare@consensys.net>
2019-10-29 15:37:36 +1000

    Cucumber needs JUnit 4

--- src/test/java/com/poc/cucumber/ValidatorReconnection.java
+++ src/test/java/com/poc/cucumber/ValidatorReconnection.java
@@ -5,6 +5,6 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = "pretty")
+@CucumberOptions(plugin = {"pretty","html:build/cucumber"})
 public class ValidatorReconnection {
 }
